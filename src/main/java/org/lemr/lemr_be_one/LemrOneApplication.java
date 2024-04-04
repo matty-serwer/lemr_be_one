504977c8788022819b76d58b6c0395a53c51f233
Lupin The Destructor <lupin@Lupins-MBP.lan>
2024-04-04 18:48:14 -0400

    Adds basic spring boot files.

---
+++ src/main/java/org/lemr/lemr_be_one/LemrOneApplication.java
@@ -0,0 +1,13 @@
+package org.lemr.lemr_be_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemrOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemrOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lemr/lemr_be_one/LemrOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemr.lemr_be_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemrOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
